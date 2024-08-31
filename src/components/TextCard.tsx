57ea63660757161c82652e84e63dc1a3ea051b95
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-09-01 02:28:11 +0330

    check the final value to see if it is very insignificant

--- src/components/TextCard.tsx
+++ src/components/TextCard.tsx
@@ -8,7 +8,9 @@ function TextCard({ finalValue }: TextCardProps) {
   return (
     <section className='space-y-2 text-center text-xl font-black md:flex md:items-center md:justify-center md:gap-6 md:space-y-0 xl:text-2xl'>
       <p>مقدار پول تبدیل شده ی شما: </p>
-      <p>{numToPersian(finalValue)}</p>
+      <p>
+        {numToPersian(finalValue) === '٠' ? 'ناچیز' : numToPersian(finalValue)}
+      </p>
     </section>
   );
 }
