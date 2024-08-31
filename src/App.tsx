16d8ce82c4320a1499205931f1e3212925f47088
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-31 07:49:22 +0330

    Add some initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import AppLayout from './components/AppLayout';
+
 function App() {
-  return <div>Hellllooo</div>;
+  return <AppLayout />;
 }
 
 export default App;
---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,11 @@
+function AppLayout() {
+  return (
+    <main className='flex h-dvh items-center justify-center'>
+      <div className='h-5/6 w-5/6 rounded bg-white/[.25] shadow-2xl backdrop-blur-lg'>
+        سلام
+      </div>
+    </main>
+  );
+}
+
+export default AppLayout;
