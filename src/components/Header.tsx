f60fbad463dbca47ad266d1294d9cf10ce8da26c
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-31 12:32:20 +0330

    Add description to the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 function Header() {
   return (
-    <header
-      dir='rtl'
-      className='mb-8 text-center text-3xl font-bold sm:mb-11 md:mb-20 md:text-4xl xl:text-5xl'
-    >
-      <h1>به سایت دلاریال خوش اومدی</h1>
+    <header dir='rtl' className='mb-8 text-center sm:mb-11 md:mb-20'>
+      <h1 className='text-3xl font-bold md:mb-3 md:text-4xl xl:mb-7 xl:text-5xl'>
+        به سایت دلاریال خوش اومدی
+      </h1>
+      <h2 className='text-sm md:text-base lg:text-lg'>
+        شما میتونید با انتخاب بین دو ارز دلار و ریال , مقدار اون یکی رو حساب
+        کنید (یک دلار = ریال 600,000)
+      </h2>
     </header>
   );
 }
